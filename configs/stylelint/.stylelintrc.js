953e8cc4779a6e0a28d6681a0364e56248d33375
ayaanlee-igaw <ayaan.lee@igaworks.com>
2023-11-21 15:37:02 +0900

    명령어 수정

--- configs/stylelint/.stylelintrc.js
+++ configs/stylelint/.stylelintrc.js
@@ -1,5 +1,3 @@
 module.exports = {
-  extends: ["stylelint-config-recommended", "stylelint-config-standard-scss"],
-  plugins: ["stylelint-order", "stylelint-value-no-unknown-custom-properties"],
   rules: {},
 };
