a062db9a4dd4813c6f9277f9f5d8e980de4328eb
ayaanlee-igaw <ayaan.lee@igaworks.com>
2023-11-22 10:04:17 +0900

    stylelint 설정

--- configs/stylelint/.stylelintrc.js
+++ configs/stylelint/.stylelintrc.js
@@ -1,3 +1,34 @@
 module.exports = {
-  rules: {},
+  extends: ["stylelint-config-standard-scss"],
+  plugins: ["stylelint-value-no-unknown-custom-properties"],
+  rules: {
+    "no-descending-specificity": null,
+    "no-duplicate-selectors": null,
+    "declaration-block-no-shorthand-property-overrides": null,
+    // "Unexpected empty line before $-variable" 규칙을 무시
+    "scss/dollar-variable-empty-line-before": null,
+    // "Unexpected () used to surround statements for @-rules" 규칙을 무시
+    "scss/at-rule-conditional-no-parentheses": null,
+    // "Unexpected invalid position @import rule" 규칙을 무시
+    "no-invalid-position-at-import-rule": null,
+    // "Expected class selector ~~~ to be kebab-case" 규칙을 무시
+    "selector-class-pattern": null,
+
+    // stylelint-value-no-unknown-custom-properties 플러그인 설정
+    "csstools/value-no-unknown-custom-properties": [
+      false,
+      // {
+      //   importFrom: [
+      //     "./src/design-system/design-tokens/design-token-css/_spacing.scss",
+      //   ],
+      //   resolver: {
+      //     extensions: [".scss"],
+      //     paths: [
+      //       "./src/design-system/design-tokens/design-token-css/_spacing.scss",
+      //     ],
+      //     moduleDirectories: ["node_modules"],
+      //   },
+      // },
+    ],
+  },
 };
